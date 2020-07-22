4af0f92aa05e4d520d966fe0012a7c5fa1a6a3bc
suarezana9 <suarezsuarezana20@gmail.com>
2020-07-22 11:20:20 -0300

    Mejoras en el diseño del slider

--- angular/src/app/pages/main/header/header.component.ts
+++ angular/src/app/pages/main/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  HostListener, OnInit } from '@angular/core';
 
 declare var jQuery:any;
 declare var $:any; 
@@ -43,5 +43,17 @@ export class HeaderComponent implements OnInit {
       }
 
   }
+
+ header_variable=false;
+
+@HostListener ("document:scroll")
+scrollfunction(){
+  if(document.body.scrollTop>0 || document.documentElement.scrollTop>0){
+    this.header_variable=true;
+  }else{
+    this.header_variable=false;
+  }
+}
+
 
 }
--- angular/src/app/pages/main/slide/slide.component.ts
+++ angular/src/app/pages/main/slide/slide.component.ts
@@ -52,7 +52,7 @@ export class SlideComponent implements OnInit {
           wrap:'.slide-inner', //Especificar el slide que vamos a usar
           isAuto: true, //Inicia la animación automáticamente
           isLoop: true, //Al finalizar vuelve a comenzar
-          interval: 7000, //Tiempo por cada slide
+          interval: 4000, //Tiempo por cada slide
           isCursor:true //Pausar animación con el mouse
 
         });
