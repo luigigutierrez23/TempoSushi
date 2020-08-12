8e3bca80c5ab4b9b28c44772ea7fae0194ce0721
suarezana9 <suarezsuarezana20@gmail.com>
2020-08-12 11:30:13 -0300

    finishing the layout

--- angular/src/app/pages/main/products/products.component.ts
+++ angular/src/app/pages/main/products/products.component.ts
@@ -1,53 +1,46 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ProductsService } from '../../../services/products.service';
 
 import { Ruta } from '../../../config';
 
-declare var jQuery:any;
-declare var $:any; 
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-
   /*---------------   Variables publicas    ----------------*/
-  public productsJson:any;
-  public renderProducts:boolean = true;
+  public productsJson: any;
+  public renderProducts: boolean = true;
   public url = Ruta.url;
+  public title_ws: string;
+  public convertir: any;
 
-  @Input() filterBy?: string ='all';
+  @Input() filterBy?: string = 'all';
 
-  constructor(private productsService : ProductsService) {
-
-      /*---------------------------------------
+  constructor(private productsService: ProductsService) {
+    /*---------------------------------------
       RECIBIENDO DATOS DINAMICOS
       --------------------------------------*/
-      productsService.getProducts()
-      .subscribe( result =>{
-        this.productsJson = result["data"];
-      })
-
-   }
-
-  ngOnInit() {
-    
+    productsService.getProducts().subscribe((result) => {
+      this.productsJson = result['data'];
+      this.convertir = this.productsJson['title'];
+      console.log(this.convertir);
+    });
   }
 
-  callback(){
+  ngOnInit() {}
 
-    if(this.renderProducts){
-
+  callback() {
+    if (this.renderProducts) {
       this.renderProducts = false;
 
-       /*---------------------------------------
+      /*---------------------------------------
        CODIGO JS
        --------------------------------------*/
-
     }
-
   }
-
 }
